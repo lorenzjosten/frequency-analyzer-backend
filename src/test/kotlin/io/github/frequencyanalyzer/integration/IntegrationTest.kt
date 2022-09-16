da50484d7d7ab660e9e82203e5c483773e2c2ea9
lorenz <lorenz.josten@gmail.com>
2022-09-16 02:23:17 +0100

    feat: add interface IntegrationTest for custom configuration

---
+++ src/test/kotlin/io/github/frequencyanalyzer/integration/IntegrationTest.kt
@@ -0,0 +1,8 @@
+package io.github.frequencyanalyzer.integration
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+interface IntegrationTest
