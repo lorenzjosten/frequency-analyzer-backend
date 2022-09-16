80aff485a69f505a595df73df61d5497bb93c4aa
lorenz <lorenz.josten@gmail.com>
2022-09-16 17:35:08 +0100

    test: add test decoder initialization

--- src/test/kotlin/io/github/frequencyanalyzer/unit/decoder/DecoderTest.kt
+++ src/test/kotlin/io/github/frequencyanalyzer/unit/decoder/DecoderTest.kt
@@ -1,6 +1,17 @@
 package io.github.frequencyanalyzer.unit.decoder
 
+import io.github.frequencyanalyzer.FileTestUtils.Companion.TEST_FILE_RESOURCE
+import io.github.frequencyanalyzer.decoder.Mp3Decoder
+import io.github.frequencyanalyzer.decoder.Mp3DecoderImpl
+import org.junit.jupiter.api.BeforeEach
+
 class DecoderTest {
 
+    lateinit var mp3Decoder: Mp3Decoder
+
+    @BeforeEach
+    fun initDecoder() {
+        mp3Decoder = Mp3DecoderImpl(TEST_FILE_RESOURCE.inputStream)
+    }
 
 }
