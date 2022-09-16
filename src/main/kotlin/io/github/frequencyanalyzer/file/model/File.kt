ceb8de69e2b567028714f9ae82ce5cd112106663
lorenz <lorenz.josten@gmail.com>
2022-09-16 02:20:39 +0100

    feat: add dto and mapper for File entity

--- src/main/kotlin/io/github/frequencyanalyzer/file/model/File.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/file/model/File.kt
@@ -2,12 +2,32 @@ package io.github.frequencyanalyzer.file.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import org.springframework.stereotype.Component
+import java.util.function.Function
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Table("file")
 data class File(
 
     @Id
-    val id: Long = 0,
+    val id: Long? = null,
+    @NotNull
+    @NotBlank
     val name: String,
+    @NotNull
+    @NotEmpty
     val data: ByteArray
+) {
+
+    fun hasId() = id != null
+}
+
+data class FileDto(
+    val id: Long?,
+    val name: String
 )
+
+@Component
+class FileMapper : Function<File, FileDto> by Function({ f -> FileDto(f.id, f.name) })
