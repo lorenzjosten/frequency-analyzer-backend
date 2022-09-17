e832eb52d1bb7d995b2aa345036bdf68c2c80309
lorenz <lorenz.josten@gmail.com>
2022-09-17 22:37:31 +0100

    feat: implement SpectralAnalysis

--- src/main/kotlin/io/github/frequencyanalyzer/spectralanalysis/model/SpectralAnalysis.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/spectralanalysis/model/SpectralAnalysis.kt
@@ -1,10 +1,49 @@
 package io.github.frequencyanalyzer.spectralanalysis.model
 
 import io.github.frequencyanalyzer.decoder.model.DecodedFrame
+import kotlin.math.log10
+import kotlin.math.sqrt
 
-class SpectralAnalysis(frame: DecodedFrame) {
+class SpectralAnalysis(private val decodedFrame: DecodedFrame) {
+
+    private val fourierTransform: DoubleArray = FourierTransform(decodedFrame).complexForward()
+
+    companion object {
+        const val DECIBEL_FACTOR = 20
+    }
 
     fun powerSpectrum(): PcmPowerSpectrum {
-        TODO("Not yet implemented.")
+        val powerSpectrum = (0 until fourierTransform.size / 4)
+            .associate {
+                coefficientOrder(it) to coefficientMagnitude(it)
+            }
+
+        return PcmPowerSpectrum(powerSpectrum)
+    }
+
+    private fun coefficientOrder(order: Int): Double {
+        val scalingFactor = decodedFrame.run { sampleFrequency.toDouble() / bufferSize }
+
+        return order * scalingFactor
+    }
+
+    private fun coefficientMagnitude(order: Int): Double {
+        val magnitude = magnitude(order)
+
+        return scaleMagnitude(magnitude)
+    }
+
+    private fun magnitude(order: Int): Double {
+        val re = fourierTransform[2 * order]
+        val im = fourierTransform[2 * order + 1]
+
+        return sqrt(re * re + im * im)
+    }
+
+    private fun scaleMagnitude(magnitude: Double): Double {
+        return when (magnitude) {
+            0.0 -> 0.0
+            else -> DECIBEL_FACTOR * log10(magnitude)
+        }
     }
 }
