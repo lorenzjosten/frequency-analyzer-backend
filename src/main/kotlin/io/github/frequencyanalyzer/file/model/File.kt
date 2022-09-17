8ff996b34647015c836589bb9721e609f378247f
lorenz <lorenz.josten@gmail.com>
2022-09-17 04:04:05 +0100

    task: implement equals, hashcode methods

--- src/main/kotlin/io/github/frequencyanalyzer/file/model/File.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/file/model/File.kt
@@ -2,7 +2,6 @@ package io.github.frequencyanalyzer.file.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
-import org.springframework.stereotype.Component
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotEmpty
 
@@ -12,7 +11,28 @@ data class File(
     @NotBlank val name: String,
     @NotEmpty val data: ByteArray
 ) {
+
     fun hasId(): Boolean = id != null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as File
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + name.hashCode()
+        result = 31 * result + data.contentHashCode()
+        return result
+    }
 }
 
 data class FileDto(
@@ -20,5 +40,4 @@ data class FileDto(
     val name: String
 )
 
-@Component
 class FileMapper : (File) -> FileDto by { FileDto(it.id, it.name) }
