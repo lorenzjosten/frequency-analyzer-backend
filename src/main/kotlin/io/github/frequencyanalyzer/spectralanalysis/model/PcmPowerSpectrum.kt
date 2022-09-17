2900afaec5a817f30a6affc90ff5e86a0af9a71d
lorenz <lorenz.josten@gmail.com>
2022-09-17 16:37:01 +0100

    feat: implement PcmPowerSpectrum

--- src/main/kotlin/io/github/frequencyanalyzer/spectralanalysis/model/PcmPowerSpectrum.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/spectralanalysis/model/PcmPowerSpectrum.kt
@@ -1,28 +1,44 @@
 package io.github.frequencyanalyzer.spectralanalysis.model
 
-private typealias CoefficientOrder = Int
-private typealias CoefficientMagnitude = Double
-private typealias FourierCoefficients = MutableMap<CoefficientOrder, CoefficientMagnitude>
+import kotlin.math.log10
+import kotlin.math.roundToInt
 
-class PcmPowerSpectrum(private val fourierCoefficients: FourierCoefficients) : Map<Int, Double> by fourierCoefficients {
+private typealias Order = Int
+private typealias Magnitude = Double
+private typealias FourierCoefficients = Map<Order, Magnitude>
+
+class PcmPowerSpectrum(
+    private val fourierCoefficients: FourierCoefficients = emptyMap()
+) : Map<Order, Magnitude> by fourierCoefficients {
 
     companion object {
         const val DECIBEL_FACTOR = 20
     }
 
     fun accumulate(other: PcmPowerSpectrum): PcmPowerSpectrum {
-        TODO("Not yet implemented")
+        val accumulated = toMutableMap()
+
+        other.map { (o, m) -> accumulated[o] = accumulated[o]?.plus(m) ?: m }
+
+        return PcmPowerSpectrum(accumulated)
     }
 
     fun scaleCoefficients(sampleRate: Int, sampleSize: Int): PcmPowerSpectrum {
-        TODO("Not yet implemented")
+        val scalingFactor = sampleRate.toDouble() / sampleSize
+        val scale: (Order) -> Order = { order: Order -> (order * scalingFactor).roundToInt() }
+        val byScaledOrder = entries.groupBy { (order, _) -> scale(order) }
+        val accumulated = byScaledOrder.mapValues { (_, toAccumulate) -> toAccumulate.sumOf { it.value } }
+
+        return PcmPowerSpectrum(accumulated)
     }
 
     fun logScaleMagnitude(): PcmPowerSpectrum {
-        TODO("Not yet implemented")
+        val scaled = mapValues { DECIBEL_FACTOR * log10(it.value) }
+
+        return PcmPowerSpectrum(scaled)
     }
 
     override fun toString(): String {
-        return fourierCoefficients.entries.toString()
+        return entries.toString()
     }
 }
