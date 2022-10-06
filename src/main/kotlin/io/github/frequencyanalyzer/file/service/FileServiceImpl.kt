87e73136b269594d2fcd2bc2f169bcb96f07e430
lorenz <lorenz.josten@gmail.com>
2022-10-07 00:23:46 +0100

    feat: add service function for reading all files, delete file

--- src/main/kotlin/io/github/frequencyanalyzer/file/service/FileServiceImpl.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/file/service/FileServiceImpl.kt
@@ -3,10 +3,11 @@ package io.github.frequencyanalyzer.file.service
 import io.github.frequencyanalyzer.file.model.File
 import io.github.frequencyanalyzer.file.repository.FileRepository
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Service
-class FileServiceImpl(val fileRepository: FileRepository) : FileService {
+class FileServiceImpl(private val fileRepository: FileRepository) : FileService {
 
     override fun save(file: File): Mono<File> {
         return fileRepository.save(file)
@@ -14,5 +15,13 @@ class FileServiceImpl(val fileRepository: FileRepository) : FileService {
 
     override fun findById(id: Long): Mono<File> {
         return fileRepository.findById(id)
+    }
+
+    override fun findAll(): Flux<File> {
+        return fileRepository.findAll()
+    }
+
+    override fun deleteById(id: Long): Mono<Void> {
+        return fileRepository.deleteById(id)
     }
 }
