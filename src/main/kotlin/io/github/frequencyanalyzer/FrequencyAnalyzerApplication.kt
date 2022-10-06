aff51f223d795158b47969634cb91168d3aed688
lorenz <lorenz.josten@gmail.com>
2022-10-07 00:24:36 +0100

    feat: import mp3 files into DB on app start up

--- src/main/kotlin/io/github/frequencyanalyzer/FrequencyAnalyzerApplication.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/FrequencyAnalyzerApplication.kt
@@ -1,11 +1,36 @@
 package io.github.frequencyanalyzer
 
+import io.github.frequencyanalyzer.file.model.File
+import io.github.frequencyanalyzer.file.service.FileService
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.support.ResourcePatternResolver
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class FrequencyAnalyzerApplication
 
 fun main(args: Array<String>) {
-	runApplication<FrequencyAnalyzerApplication>(*args)
+    runApplication<FrequencyAnalyzerApplication>(*args)
+}
+
+@Component
+class SetupData(
+    private val resourceResolver: ResourcePatternResolver,
+    private val fileService: FileService
+) : ApplicationRunner {
+
+    override fun run(args: ApplicationArguments?) {
+        readFiles().map(fileService::save).map(Mono<File>::block)
+    }
+
+    private fun readFiles(): List<File> {
+        return resourceResolver
+            .getResources("classpath:mp3/*.mp3")
+            .map { it.file }
+            .map { File(name = it.name, data = it.readBytes()) }
+    }
 }
