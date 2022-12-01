4fd1094d932c90fd34fb03977517df8f88b988bb
lorenz <lorenz.josten@gmail.com>
2022-12-01 19:33:16 +0000

    feat: use proper resource resolution for DB population

--- src/main/kotlin/io/github/frequencyanalyzer/FrequencyAnalyzerApplication.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/FrequencyAnalyzerApplication.kt
@@ -6,12 +6,12 @@ import io.github.frequencyanalyzer.track.model.TrackDataRepository
 import io.github.frequencyanalyzer.track.model.TrackRepository
 import io.github.frequencyanalyzer.upload.model.Upload
 import io.r2dbc.spi.Blob
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.core.io.Resource
-import org.springframework.core.io.support.ResourcePatternResolver
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import reactor.core.publisher.Flux
@@ -28,35 +28,39 @@ fun main(args: Array<String>) {
 
 @Component
 class SetupData(
-    private val resourceResolver: ResourcePatternResolver,
     private val tracks: TrackRepository,
-    private val trackData: TrackDataRepository
+    private val trackData: TrackDataRepository,
+    @Value("classpath:mp3/1kHz-5sec.mp3") val _1kHz: Resource,
+    @Value("classpath:mp3/10kHz-5sec.mp3") val _10kHz: Resource,
+    @Value("classpath:mp3/100Hz-5sec.mp3") val _100Hz: Resource,
+    @Value("classpath:mp3/250Hz-5sec.mp3") val _250Hz: Resource,
+    @Value("classpath:mp3/440Hz-5sec.mp3") val _440Hz: Resource
 ) : ApplicationRunner {
 
     override fun run(args: ApplicationArguments?) {
-        readFiles().flatMap { saveUploadData(it) }.subscribe()
+        readFiles().flatMap { persistTracks(it) }.subscribe()
     }
 
     @Transactional
-    fun saveUploadData(upload: Upload): Mono<TrackData> {
-        return createTrack(upload).flatMap { createTrackData(it, upload) }
+    fun persistTracks(file: Upload): Mono<TrackData> {
+        return createTrack(file).flatMap { createTrackData(it, file) }
     }
 
-    fun createTrack(upload: Upload): Mono<Track> {
-        val track = Track(name = upload.name)
+    fun createTrack(file: Upload): Mono<Track> {
+        val track = Track(name = file.name)
         return tracks.save(track)
     }
 
-    fun createTrackData(track: Track, upload: Upload): Mono<TrackData> {
-        val data = TrackData(track.id!!, upload.data)
+    fun createTrackData(track: Track, file: Upload): Mono<TrackData> {
+        val data = TrackData(track.id!!, file.data)
         return trackData.save(data)
     }
 
     private fun readFiles(): Flux<Upload> {
         return resources()
             .map {
-                val file = it.file
-                val data = ByteBuffer.wrap(file.readBytes())
+                val stream = it.inputStream
+                val data = ByteBuffer.wrap(stream.readAllBytes())
                 val blob = Blob.from(Mono.just(data))
                 Upload(name = it.filename!!, data = blob)
             }
@@ -64,6 +68,6 @@ class SetupData(
     }
 
     private fun resources(): Flux<Resource> {
-        return resourceResolver.getResources("classpath:mp3/*.mp3").toFlux()
+        return Flux.just(_1kHz, _440Hz, _10kHz, _100Hz, _250Hz)
     }
 }
