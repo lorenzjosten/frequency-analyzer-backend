5a4e9996a583fffdd7359b9580c5779d017e86c4
lorenz <lorenz.josten@gmail.com>
2022-12-01 13:39:57 +0000

    feat: add TrackPersistenceException

--- src/main/kotlin/io/github/frequencyanalyzer/track/error/TrackErrorHandler.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/track/error/TrackErrorHandler.kt
@@ -8,13 +8,22 @@ import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
 
 @Configuration
-class TrackErrorFilter {
+class TrackErrorHandler {
 
     @Bean
     fun trackNotFound() = WebFilter { exchange: ServerWebExchange, chain: WebFilterChain ->
         chain.filter(exchange)
                 .onErrorResume(TrackNotFoundException::class.java) {
                     exchange.response.statusCode = HttpStatus.NOT_FOUND
+                    exchange.response.setComplete()
+                }
+    }
+
+    @Bean
+    fun trackNotPersisted() = WebFilter { exchange: ServerWebExchange, chain: WebFilterChain ->
+        chain.filter(exchange)
+                .onErrorResume(TrackPersistenceException::class.java) {
+                    exchange.response.statusCode = HttpStatus.BAD_REQUEST
                     exchange.response.setComplete()
                 }
     }
--- src/main/kotlin/io/github/frequencyanalyzer/track/error/TrackErrors.kt
+++ src/main/kotlin/io/github/frequencyanalyzer/track/error/TrackErrors.kt
@@ -4,3 +4,4 @@ sealed class TrackError(override val message: String) : RuntimeException()
 
 class TrackNotFoundException(id: Long) : TrackError("Cannot find track with id $id.")
 
+class TrackPersistenceException(name: String) : TrackError("Cannot persist track $name.")
